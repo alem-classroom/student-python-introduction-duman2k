7411e41a4c692678c349bf5316813a312cb934e3
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 02:11:38 +0600

    feat: solved loops-for

--- loops-for/arrayLoop.py
+++ loops-for/arrayLoop.py
@@ -1,2 +1,5 @@
 def insert_squares(arr, num):
     # add square of numbers from 1 to num to the list named arr and return list
+    for n in range(1, num + 1):
+        arr.append(n*n)
+    return arr
--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,2 +1,10 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
+    keys = list(dict.keys())
+    values = list(dict.values())
+    i = 0
+    for item in values:
+        dict[item] = keys[i]
+        dict.pop(keys[i])
+        i += 1
+    return dict
