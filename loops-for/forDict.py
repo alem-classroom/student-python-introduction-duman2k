d855cbf9264fccc6e3c7ced5f46d643755d9a446
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 03:39:43 +0600

    feat: loops-for/forDict.py solved

--- loops-for/forDict.py
+++ loops-for/forDict.py
@@ -1,7 +1,3 @@
 def reverse_dict(dict):
     # swap keys and values within dict and return dict
-    for key in list(dict.keys()):
-        dict[dict[key]] = key
-        dict.pop(key)
-        
-    return dict
+    return {value:key for key, value in dict.items()}
