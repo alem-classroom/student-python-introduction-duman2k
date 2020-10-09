a7d77593656d5d3b10b56a1df13fe8e00218caae
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 20:04:09 +0600

    feat: list solved

--- lists/lists.py
+++ lists/lists.py
@@ -3,14 +3,13 @@ def size_of_list(list):
     return len(list)
 
 def add_elem_to_list(list, elem):
-     # add elem to list and return the list
+    # add elem to list and return the list
     list.append(elem)
     return list
 
 def delete_elem_from_list(list, index = -1):
-     # delete element from list, such that its index is index
-    list.pop(index)
-    return list
+    # delete element from list, such that its index is index
+    return list.pop(index)
 
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
