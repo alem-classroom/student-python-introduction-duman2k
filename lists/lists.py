42836f13ca46d43a9dbf3d88e5f7da5a96a5b7aa
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 19:51:43 +0600

    feat: list solved

--- lists/lists.py
+++ lists/lists.py
@@ -9,13 +9,16 @@ def add_elem_to_list(list, elem):
 
 def delete_elem_from_list(list, index = -1):
      # delete element from list, such that its index is index
-    return list.pop(index)
+    list.pop(index)
+
 def count_elements_in_list(list, x):
     # count elements in the list that are equal to x and return the count
     return list.count(x)
 def sort_list(list):
     # return sorted list
-    return list.sort()
+    list.sort()
+    return list
 def reverse(list):
     # return reversed list
-    return list.reverse()
+    list.reverse()
+    return list
