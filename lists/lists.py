d43fb7ef32d4b7df1fa946f2eee493a6ff6f5481
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 17:21:53 +0600

    feat: lists resolved

--- lists/lists.py
+++ lists/lists.py
@@ -9,7 +9,10 @@ def add_elem_to_list(list, elem):
 
 def delete_elem_from_list(list, index = -1):
     # delete element from list, such that its index is index
-    list.pop(index)
+    if index < len(list):
+        list.pop(index)
+        return list
+    list.pop()
     return list
 
 def count_elements_in_list(list, x):
