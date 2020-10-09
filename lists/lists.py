49dc6a4eaa382039c79b989fe534d4e72b3b9711
duman2k <55402925+duman2k@users.noreply.github.com>
2020-10-09 19:29:48 +0600

    feat: list resolved

--- lists/lists.py
+++ lists/lists.py
@@ -1,21 +1,22 @@
 def size_of_list(list):
     # return size of list
-    return len(list)
+     return len(list)
 
-def add_elem_to_list(list, elem):
-     # add elem to list and return the list
-     list.append(elem)
-     return list
+ def add_elem_to_list(list, elem):
+      # add elem to list and return the list
+      list.append(elem)
+      return list
 
-def delete_elem_from_list(list, index = -1):
-     # delete element from list, such that its index is index
-    return list.pop(index)
-def count_elements_in_list(list, x):
-    # count elements in the list that are equal to x and return the count
-    return list.count(x)
-def sort_list(list):
-    # return sorted list
-    return list.sort()
-def reverse(list):
-    # return reversed list
-    return list.reverse()
+ def delete_elem_from_list(list, index = -1):
+      # delete element from list, such that its index is index
+     return list.pop(index)
+ def count_elements_in_list(list, x):
+     # count elements in the list that are equal to x and return the count
+     return list.count(x)
+
+ def sort_list(list):
+     # return sorted list
+     return list.sort()
+ def reverse(list):
+     # return reversed list
+     return list.reverse() 
