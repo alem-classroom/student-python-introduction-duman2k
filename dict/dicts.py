f895c10cbfc47bf56289c8265bcfab3a5e4b8655
Думан <duman2k@gmail.com>
2020-10-28 16:06:17 +0600

    feat: dicts solved

--- dict/dicts.py
+++ dict/dicts.py
@@ -17,7 +17,14 @@ def get_dict_value_by_key(dict, key):
 
 def delete_dict_element_by_key(dict, key):
     # delete and return element from dict, such that its key is the argument key
-    if key in dict:
-        dict.pop(key)
-        return dict
-    return
+    return dict.pop(key)
+    
+
+    #if key in dict:
+    #    dict.pop(key)
+    #return dict
+
+    #if key in dict:
+    #    dict.pop(key)
+    #    return dict
+    #return
